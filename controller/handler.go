26ad2af23e83da84a07bbd8bfbc2f52b800862da
gene891212 <gene891212@gmail.com>
2021-05-06 01:28:19 +0800

    refactor echobot handler

--- controller/handler.go
+++ controller/handler.go
@@ -4,8 +4,11 @@ import (
 	"database/sql"
 	"linebot-server/database"
 	"linebot-server/models"
+	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 type Repository struct {
@@ -57,3 +60,44 @@ func (handler *Repository) GetUsersHandler(c *gin.Context) {
 // func (handler *Repository) CreateUserHandler(c *gin.Context) {
 
 // }
+
+func (handler *Repository) EchoBot(c *gin.Context) {
+	bot, err := linebot.New(
+		os.Getenv("CHANNEL_SCRECT"),
+		os.Getenv("ACCESS_TOKEN"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	events, err := bot.ParseRequest(c.Request)
+	if err != nil {
+		panic(err)
+	}
+	for _, event := range events {
+		if event.Type == linebot.EventTypeFollow {
+			userId := event.Source.UserID
+			profile, err := bot.GetProfile(userId).Do()
+			if err != nil {
+				c.JSON(500, gin.H{
+					"status":  500,
+					"message": err.Error(),
+				})
+				return
+			}
+			models.CreateUser(handler.Db, profile)
+		} else if event.Type == linebot.EventTypeMessage {
+			switch msg := event.Message.(type) {
+			case *linebot.TextMessage:
+				_, err := bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(msg.Text)).Do()
+				if err != nil {
+					c.JSON(500, gin.H{
+						"status":  500,
+						"message": err,
+					})
+					return
+				}
+			}
+		}
+	}
+}
